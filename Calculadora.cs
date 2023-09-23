b4409d1de021c7495987c46c61ca1d93726696e5
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2023-09-23 08:58:26 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,41 @@
+﻿namespace desafio_testes_unitarios_TDD_DIO
+{
+    public class Calculadora
+    {
+        private List<string> _historico = new List<string>();
+
+        public int Soma(int numero1, int numero2)
+        {
+            int resultado = numero1 + numero2;
+            _historico.Insert(0, $"resultado: {resultado}");
+            return resultado;
+        }
+
+        public int Subtracao(int numero1, int numero2)
+        {
+            int resultado = numero1 - numero2;
+            _historico.Insert(0, $"resultado: {resultado}");
+            return resultado;
+        }
+
+        public int Multiplicacao(int numero1, int numero2)
+        {
+            int resultado = numero1 * numero2;
+            _historico.Insert(0, $"resultado: {resultado}");
+            return resultado;
+        }
+
+        public int Divisao(int numero1, int numero2)
+        {
+            int resultado = numero1 / numero2;
+            _historico.Insert(0, $"resultado: {resultado}");
+            return resultado;
+        }
+
+        public List<string> RetornaTresUltimosHistoricos()
+        {
+            _historico.RemoveRange(3, _historico.Count - 3);
+            return _historico;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using desafio_testes_unitarios_TDD_DIO;
+
+Calculadora calculadora = new Calculadora();
+
+int num1 = 5;
+int num2 = 10;
+
+Console.WriteLine($"{num1} + {num2} = {calculadora.Soma(num1, num2)}");
